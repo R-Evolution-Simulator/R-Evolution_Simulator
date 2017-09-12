4406102aa3e87e8ee7c004a1c8e7d75fa4b9fcd6
MrSosso <alesosso@gmail.com>
2017-09-12 16:48:18 +0200

    many many Docstrings

--- classes/chunk.py
+++ classes/chunk.py
@@ -2,25 +2,25 @@ from noise.simplexnoise.noise import normalize
 
 
 class Chunk:
-    '''classe dei chunk ( unita' del territorio )'''
+    """class of chunk (unity of territory)"""
 
     def __init__(self, world, x, y):
-        '''costruttore dei chunk'''
+        '''construcot of Chunk'''
         self.coord = [x, y]
 
-        # cibo
+        # food
         self.world = world
         self.foodMax = normalize(self.world.ch_foodMaxNoise.noise(x, y)) * self.world.ch_foodMaxMax  # l'erba massima in un territorio e' compresa tra 0 e max ed e' fissa
         self.food = self.foodMax / 2  # all'inizio il cibo e' al massimo
         self.growthRate = self.foodMax * self.world.ch_growthCoeff  # la crescita e' direttamente proporzionale all'erba massima
 
-        # temperatura
+        # temperature
         self.temperature = (normalize(self.world.ch_temperatureNoise.noise(x, y)) - 0.5) * self.world.ch_temperatureMax * 2  # la temperatura in un territorio e' compresa tra -max e max ed e' fissa
         self.foodHistory = ""
         self.chunkCreatureList = []
 
     def update(self):
-        '''metodo di update dei chunk'''
+        '''chunk update method'''
         self.food *= (1 + self.growthRate)  # ad ogni ciclo l'erba cresce
         if self.food > self.foodMax:  # finche' non raggiunge il massimo
             self.food = self.foodMax
@@ -28,6 +28,7 @@ class Chunk:
         self.foodHistory += f"{int(self.food)},"
 
     def __del__(self):
+        """chunk destructor"""
         try:
             self.world.chunkData.write(f"{self.coord[0]};{self.coord[1]};{self.foodMax};{self.growthRate};{self.temperature};{self.foodHistory[:-1]}\n")
         except ValueError:
--- classes/creature.py
+++ classes/creature.py
@@ -7,25 +7,25 @@ from random import random as rnd
 
 
 class Creature:
-    '''classe degli organismi'''
+    '''class of creatures'''
 
     def __init__(self, world, x, y, parentsID, energy, tempResistGen, agility, bigness, sex, fertility, numControlGene, startCount=0):
-        '''costruttore degli organismi'''
+        '''creatures constructor'''
         self.world = world
         self.ID = self.world.cr_creaturesIDCount
-        self.coord = [x, y]  # definite le coord iniziali dell'orgsnismo
+        self.coord = [x, y]  # creatture's starting coord definition
         self.parentsID = parentsID
         self.tickHistory = ""
-        self.birthTick = self.world.tickCount - startCount  # registrato il tick di creazione dell'orgsnismo (startCount serve solo per la diversificazione nel setup iniziale)
-        self.energy = energy  # definite l'energia iniziale dell'orgsnismo
-        self.chunkIn().chunkCreatureList.append(self)  # aggiunto l'organismo alla lista degli organismi presenti nel chunk in cui si trova
-        self.reprodReady = False  # settata la capacita' riproduttiva su falso
-        self.deathDate = int(random.gauss(self.world.cr_averageAge, self.world.cr_deviationAgeProb))  # stabilita' l'eventuale eta' di morte per vcchiaiadell'organismo
-        self.age = 0  # settata l'eta' a 0
+        self.birthTick = self.world.tickCount - startCount  # creature's creation tick definition (startCount is used only during diversification at start)
+        self.energy = energy  # creature's starting energy definition
+        self.chunkIn().chunkCreatureList.append(self)  # creature's adding to the list of creatures in its chunk
+        self.reprodReady = False  # reproduction capacity set to false
+        self.deathDate = int(random.gauss(self.world.cr_averageAge, self.world.cr_deviationAgeProb))  # crearture's age of death definition
+        self.age = 0  # age set to 0
         self.destChunk = [self.chunkCoord(0), self.chunkCoord(1)]
 
-        # attribuiti i geni alla creatura
-        self.tempResistGen = tempResistGen  # puo' essere N (normale), l (pelo lungo), c (pelo corto)
+        # creature's genes definition
+        self.tempResistGen = tempResistGen  # it can be N (normal), c (short fur), l (long fur)
         self.agility = agility
         self.bigness = bigness
         self.sex = sex
@@ -33,17 +33,17 @@ class Creature:
         self.reprodCountdown = fertility
         self.numControlGene = numControlGene
 
-        # calcolo caratteristiche fenotipiche creatura
+        # phenotypic characteristics valuation
         self.tempResist = self.tempResistCalc(self.tempResistGen)
         self.speed = (self.agility / self.bigness) * 2
         self.eatCoeff = self.bigness * self.world.cr_eatCoeffMax
 
         self.world.cr_creaturesIDCount += 1
 
-        self.world.newBorn.add(self)  # organismo aggiunto alla lista degli organismi
-        # print(world, x, y, energy, tempResistGen, agility, bigness, sex, fertility, birthFrame, startCount)
+        self.world.newBorn.add(self)  # creature's adding to the list of creatures
 
     def __del__(self, cause="e"):
+        """creature's destructor"""
         self.deathTick = self.world.tickCount
         self.deathCause = cause
         try:
@@ -53,9 +53,9 @@ class Creature:
             pass
 
     def update(self):
-        '''metodo di update, AI degli organismi'''
+        """creature update/AI method"""
 
-        # controllo per la riproduzione
+        # reproduction control
         if self.energy > 50 and self.reprodCountdown <= 0:
             self.reprodReady = True
             self.datingAgency()
@@ -64,43 +64,43 @@ class Creature:
             self.reprodReady = False
             self.reprodCountdown -= 1
 
-        # calcolo e controllo movimento verso cibo
+        # food search
         self.destCalc()
         if not [self.chunkCoord(0),
-                self.chunkCoord(1)] == self.destChunk:  # se non si trova gia' nella destinazione, si muove...
+                self.chunkCoord(1)] == self.destChunk:
             self.step()
 
-        else:  # ...altrimenti mangia
+        else:
             self.eat()
 
-        # update variabili della creatura
-        self.energy -= self.world.cr_enDecCoeff * self.energy  # tolta energia al passare di ogni ciclo
+        # creature's variables update
+        self.energy -= self.world.cr_enDecCoeff * self.energy  # energy decrease every tick
         self.age += 1
 
-        # controllo morte
+        # death control
         self.tickHistory += f"{round(self.coord[0],2)},{round(self.coord[1],2)},{int(self.energy)},{int(self.reprodReady)}/"
         self.deathControl()
 
     def chunkIn(self):
-        """metodo che restituisce l'oggetto Chunk nel quale si trova la creatura"""
+        """returns Chunk object in which the creature is"""
         return self.world.chunkList[self.chunkCoord(0)][self.chunkCoord(1)]
 
     def deathControl(self):
-        '''metodo di controllo delle condizioni di morte dell'organismo'''
-        if self.energy < 10:  # morte di fame
+        '''death control method'''
+        if self.energy < 10:  # starvation death
             self.world.tickDead.add(self)
             self.__del__("s")
 
-        elif rnd() <= self.deathProbTemp():  # morte per temperatura
+        elif rnd() <= self.deathProbTemp():  # temperature death
             self.world.tickDead.add(self)
             self.__del__("t")
 
-        elif self.age >= self.deathDate:  # morte di vecchiaia
+        elif self.age >= self.deathDate:  # death by age
             self.world.tickDead.add(self)
             self.__del__("a")
 
     def deathProbTemp(self):
-        '''calcolo della probabilita' di morire per la temperatura'''
+        '''temperature death probabilities calc'''
         t = self.world.ch_temperatureMax
         if self.tempResist == "c":
             return ((self.chunkIn().temperature ** 2 / (4 * (self.world.ch_temperatureMax ** 2))) - (self.chunkIn().temperature / (2 * self.world.ch_temperatureMax)) + (1 / 4)) / self.world.cr_tempDeathProbCoeff
@@ -112,95 +112,89 @@ class Creature:
             return ((self.chunkIn().temperature ** 2) / (self.world.ch_temperatureMax ** 2)) / self.world.cr_tempDeathProbCoeff
 
     def destCalc(self):
-        '''calcolo del chunk piu' conveniente agli animali'''
+        '''most convenient chunk calc'''
 
         x = self.chunkCoord(0)
         y = self.chunkCoord(1)
         maxEn = float("-inf")
 
         for i in range(max(x - self.world.cr_viewRay, 0), min(x + self.world.cr_viewRay + 1, int(
-                        (self.world.width) / self.world.chunkDim))):  # si cicla all'interno del quadrato...
+                        (self.world.width) / self.world.chunkDim))):
             for j in range(max(y - self.world.cr_viewRay, 0), min(y + self.world.cr_viewRay + 1, int(
-                            (self.world.height) / self.world.chunkDim))):  # ...dei Chunk delimitati dal raggio di vista
+                            (self.world.height) / self.world.chunkDim))):
 
                 if self.world.chunkList[i][j].food * self.eatCoeff * self.world.cr_enIncCoeff - self.energyConsume(i,
-                                                                                                                   j) > maxEn:  # se l'energia guadagnata in una certa zona meno quella spesa per andarci e' migliore:
+                                                                                                                   j) > maxEn:
                     maxEn = self.world.chunkList[i][
                                 j].food * self.eatCoeff * self.world.cr_enIncCoeff - self.energyConsume(i, j)
-                    self.destChunk = [i, j]  # si aggiorna la variabile destinazione in chunk
+                    self.destChunk = [i, j]
 
         self.destCoord = [(self.destChunk[0] + 0.5) * self.world.chunkDim, (
-            self.destChunk[1] + 0.5) * self.world.chunkDim]  # e in pixel (che corrisponde al centro di un Chunk)
+            self.destChunk[1] + 0.5) * self.world.chunkDim]
 
     def step(self):
-        '''metodo che calcola la nuova posizione dopo uno spostamento ogni tick'''
+        '''creature's new position calc'''
 
         self.chunkIn().chunkCreatureList.remove(
-            self)  # l?organismo si leva dalla lista degli organismi del chunk in cui e', ...
+            self)
 
         self.coord[0] += (self.destCoord[0] - self.coord[0]) / math.sqrt((self.destCoord[0] - self.coord[0]) ** 2 + (
-            self.destCoord[1] - self.coord[1]) ** 2) * self.speed  # ...calcola la nuova destinazione,...
+            self.destCoord[1] - self.coord[1]) ** 2) * self.speed
         self.coord[1] += (self.destCoord[1] - self.coord[1]) / math.sqrt(
             (self.destCoord[0] - self.coord[0]) ** 2 + (self.destCoord[1] - self.coord[1]) ** 2) * self.speed
 
         self.chunkIn().chunkCreatureList.append(
-            self)  # ... e si aggiunge alla nuova lista
+            self)
 
     def eat(self):
-        '''metodo che aggiorna la quantita' di cibo nel chunk e l'energia dell'organismo in un "boccone" dell'organismo'''
+        '''energy and chunk's remaining food update'''
         foodEaten = self.chunkIn().food * self.eatCoeff
-        self.energy += foodEaten * self.world.cr_enIncCoeff  # qui l'energia aumenta del cibo mangiato per il coefficiente di aumento dell'energia
-        self.chunkIn().food -= foodEaten  # si diminuisce il cibo sul prato
-        self.energy = min(self.energy, 100)  # l'energia viene limitata a 100
+        self.energy += foodEaten * self.world.cr_enIncCoeff
+        self.chunkIn().food -= foodEaten
+        self.energy = min(self.energy, 100)
 
     def energyConsume(self, x, y):
-        '''metodo che restituisce l'enrgia persa per raggiungere un chunk'''
+        '''energy consumption to reach a chunk'''
 
         return (math.sqrt((x * self.world.chunkDim + 5 - self.coord[0]) ** 2 + (
             y * self.world.chunkDim + 5 - self.coord[1]) ** 2) / self.speed) * self.world.cr_enDecCoeff * self.energy
 
     def chunkCoord(self, i):
-        '''converte le coordinate nel chunk che le contiene'''
+        '''returns chunk cord of the creature chunk'''
         if i == 0:
             return min(int(self.coord[i] / self.world.chunkDim), int(self.world.width / self.world.chunkDim) - 1)
         else:
             return min(int(self.coord[i] / self.world.chunkDim), int(self.world.height / self.world.chunkDim) - 1)
 
     def datingAgency(self):
-        '''metodo con cui le creature controllano se nella lista del chunk ne sono presenti altre pronte alla riproduzione; se si', si riproducono'''
+        '''reproduction mate finder in the chunk. If there is someone "available", the creature reproduces itself'''
 
-        for i in self.chunkIn().chunkCreatureList:  # cicla tra tutte le crature del chunk
+        for i in self.chunkIn().chunkCreatureList:
 
-            if i.reprodReady and i.sex != self.sex:  # controlla se sono pronte alla riproduzione
+            if i.reprodReady and i.sex != self.sex:
 
                 self.reproduction(i)
                 self.reprodReady = False
                 i.reprodReady = False
 
     def reproduction(self, shelf):
-        '''metodo della riproduzione delle creature: ne viene creata una nuova con le caratteristiche genetiche ereditate dai genitori'''
+        '''reproduction method'''
 
-        # calcolo delle caratteristiche della nuova creatura
-        x = (self.coord[0] + shelf.coord[0]) / 2  # calcolo della posizione x
-        y = (self.coord[1] + shelf.coord[1]) / 2  # calcolo della posizione y
-        energy = (self.energy + shelf.energy) / 2  # calcolo dell'energia
+        x = (self.coord[0] + shelf.coord[0]) / 2
+        y = (self.coord[1] + shelf.coord[1]) / 2
+        energy = (self.energy + shelf.energy) / 2
         tempResistGen = self.tempResistGen[int(rnd() * 2)] + shelf.tempResistGen[
-            int(rnd() * 2)]  # calcolo della resistenza alla temperatura
-        agility = (self.agility + shelf.agility) / 2 + random.gauss(0, self.world.cr_mutationCoeff)  # calcolo dell'agilita'
-        bigness = (self.bigness + shelf.bigness) / 2 + random.gauss(0, self.world.cr_mutationCoeff)  # calcolo della grandezza
-        sex = int(rnd() * 2)  # calcolo del sesso
-        fertility = (self.fertility + shelf.fertility) / 2 + random.gauss(0, self.world.cr_mutationCoeff)  # calcolo della fertilita'
+            int(rnd() * 2)]
+        agility = (self.agility + shelf.agility) / 2 + random.gauss(0, self.world.cr_mutationCoeff)
+        bigness = (self.bigness + shelf.bigness) / 2 + random.gauss(0, self.world.cr_mutationCoeff)
+        sex = int(rnd() * 2)
+        fertility = (self.fertility + shelf.fertility) / 2 + random.gauss(0, self.world.cr_mutationCoeff)
         numControlGene = (self.numControlGene + shelf.numControlGene) / 2 + random.gauss(0, self.world.cr_mutationCoeff)
 
-        Creature(self.world, x, y, (self.ID, shelf.ID), energy, tempResistGen, agility, bigness, sex, fertility, numControlGene)  # generazione della nuova creature con le caratteristiche calcolate
-        # Creature.tickBorn += 1  # aggiunto 1 ai nati nel tick
-
-    '''def geneControl(self, v, vmin, vmax):
-        metodo di controllo dei geni: devono rientrare entro i valori limite
-        return max(vmin, min(vmax, v))'''
+        Creature(self.world, x, y, (self.ID, shelf.ID), energy, tempResistGen, agility, bigness, sex, fertility, numControlGene)
 
     def tempResistCalc(self, gen):
-        '''calcolo dell'espressione fenotipica del gene della resistenza alla temperatura'''
+        '''creature's phenotipyc expression of the temperature calc'''
         if gen[0] == "N" or gen[1] == "N":
             return "N"
 
