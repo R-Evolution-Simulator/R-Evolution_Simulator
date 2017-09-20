012435b588c6c9bcc279b040fd14e844de0440ca
MrSosso <alesosso@gmail.com>
2017-09-20 15:33:46 +0200

    Lol

---
+++ classes/noise/examples/1D_image (2).py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from simplexnoise.noise import PerlinNoise, normalize
+
+length = 100
+pn = PerlinNoise(num_octaves=7, persistence=0.1)
+data = []
+t = [i for i in xrange(length)]
+for i in xrange(length):
+    data.append(normalize(pn.fractal(x=i, hgrid=length)))
+
+fig = plt.figure()
+plt.plot(t, data)
+fig.savefig('1D_example.png')
