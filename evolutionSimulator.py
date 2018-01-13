5e41fc652eeafc2ed6952a8c35cbe7be2823148f
MatteoPalmieri <matteo@studioingegneriamp.it>
2018-01-13 21:21:53 +0100

    c

--- evolutionSimulator.py
+++ evolutionSimulator.py
@@ -5,7 +5,7 @@ name = "test"
 lol = var.DFEAULT_SIM_VARIABLES
 lol['dimension'] = (60,45)
 lol['initial_creatures'] = 150
-lol['max_lifetime'] = 2000
+lol['max_lifetime'] = 3000
 world = World(name, lol)
 world.run()
 del(world)
