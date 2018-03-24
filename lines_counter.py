a206d2f5d91ba1e4976ff45ff3d3ff780bf5fa31
MrSosso <Ramanujan1887>
2018-03-24 13:25:46 +0100

    Lines counter

--- lines_counter.py
+++ lines_counter.py
@@ -3,12 +3,13 @@ import os
 tot = 0
 for i in os.listdir(os.path.join(os.getcwd(), 'modules')):
     print(i, end=' - ')
+    lines = 0
     try:
         f = open(os.path.join(os.getcwd(), 'modules', i))
     except IsADirectoryError:
         pass
     else:
         lines = len(f.readlines())
-        print(lines)
         tot += lines
+    print(lines)
 print(tot)
