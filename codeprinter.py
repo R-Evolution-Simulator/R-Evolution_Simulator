ad3838be10ae3f4016a98f0e3257d2ca739e6334
MrSosso <Ramanujan1887>
2018-03-25 12:41:31 +0200

    Lines counter

--- codeprinter.py
+++ codeprinter.py
@@ -1,18 +1,28 @@
 """
-Just some pointless lines that print the code in files "world.py", "creature.py", "chunk.py", "analysis.py", "graphics.py"
+Just some pointless lines that print the code in files
 """
 
 from time import *
+import os
 
-name = ["world", "creature", "chunk", "analysis", "graphics"]
 
-lines = {}
-for a in name:
-    f = open(f"modules/{a}.py")
-    lines[a] = f.readlines()
+def print_and_wait(str):
+    print(str)
+    sleep(0.1)
+
+
+modules = os.listdir(os.path.join(os.getcwd(), 'modules'))
+lines = dict()
+for a in modules:
+    try:
+        f = open(os.path.join(os.getcwd(), 'modules', a))
+    except IsADirectoryError:
+        pass
+    else:
+        lines[a] = f.readlines()
 while True:
-    for a in name:
-        print(f"----- file: {a}.py -----")
+    for a in lines:
+        for i in ('', '', '', '', f"----- file: {a} -----", '', ''):
+            print_and_wait(i)
         for line in lines[a]:
-            print(line, end='')
-            sleep(0.05)
+            print_and_wait(line[:-1])
