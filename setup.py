065fcf30cfbde07aaba5ad57cb9c44bd2964eec9
r-evolution-simulator <revolution.simulator@gmail.com>
2018-03-29 10:40:15 +0200

    created setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
- 
+from setuptools import setup
+
+if __name__ == "__main__":
+
+    setup(
+        name = "R-Evolution Simulator",
+        version='1.0.0',
+        packages = ['modules','modules.noise','modules.noise.examples','modules.noise.img','modules.noise.simplexnoise',
+                    'modules.noise.tests','data','data.templates','data.sounds.cows',
+                    ],
+        scripts=['R-Evolution_Simulator.py'],
+        author = "Federico Malnati, Matteo Palmieri, Alessandro Sosso",
+        author_email = "revolution.simulator@gmail.com",
+        url = "https://r-evolution-simulator.github.io/",
+        install_requires=[
+            'scipy',
+            'numpy',
+            'Pillow',
+            'pygame',
+            'matplotlib'
+        ],
+)
